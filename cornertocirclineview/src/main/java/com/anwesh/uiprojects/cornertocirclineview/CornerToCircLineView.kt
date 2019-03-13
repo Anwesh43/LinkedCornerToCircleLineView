441945b8cc454273721727b663cf7c63d6c9f152
Anwesh43 <anweshthecool0@gmail.com>
2019-03-14 01:58:26 +0530

    adding Int and Float extension methods

--- cornertocirclineview/src/main/java/com/anwesh/uiprojects/cornertocirclineview/CornerToCircLineView.kt
+++ cornertocirclineview/src/main/java/com/anwesh/uiprojects/cornertocirclineview/CornerToCircLineView.kt
@@ -21,3 +21,13 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#388E3C")
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+fun Float.crx(deg : Float) : Float = this * Math.cos(deg * Math.PI/180).toFloat()
+fun Float.cry(deg : Float) : Float = this * Math.sin(deg * Math.PI/180).toFloat()
+fun Float.updateToD(d : Float, sc : Float) : Float = this + (d - this) * sc
