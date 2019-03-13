b54cd45de4f730262470945f840b7600989b6f7e
Anwesh43 <anweshthecool0@gmail.com>
2019-03-14 01:52:59 +0530

    adding libraries and constants

--- cornertocirclineview/src/main/java/com/anwesh/uiprojects/cornertocirclineview/CornerToCircLineView.kt
+++ cornertocirclineview/src/main/java/com/anwesh/uiprojects/cornertocirclineview/CornerToCircLineView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.cornertocirclineview
 /**
  * Created by anweshmishra on 14/03/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#388E3C")
+val backColor : Int = Color.parseColor("#BDBDBD")
