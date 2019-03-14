a722c0a9892d7d64636c222b09db3476ef101d9d
Anwesh43 <anweshthecool0@gmail.com>
2019-03-14 09:56:26 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcornertocirclineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcornertocirclineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedcornertocirclineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.cornertocirclineview.CornerToCircLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CornerToCircLineView.create(this)
     }
 }
